ba9ef580173fb4fb7fd49eb63d8f6b95d288d492
tang huayi <huayi.tang@etu.unistra.fr>
2023-01-09 02:07:34 +0100

    Defines Models for books

---
+++ books/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# talks/models.py
+from pyexcel import get_sheet
+from django.db import models
+
+#Create a model for gutenberg_data.csv
+
+class Book(models.Model):
+    #Title, Author, Link, ID, Bookshelf, Text
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    link = models.URLField()
+    #book_id = models.IntegerField()
+    bookshelf = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    @classmethod
+    def populate(cls):
+        sheet = get_sheet(file_name="gutenberg_data.csv")
+        for row in sheet:
+            book = cls(title=row[0], author=row[1], link=row[2], bookshelf=row[4], text=row[5])
+            book.save()
+
+
+
+
+
+
+
+
