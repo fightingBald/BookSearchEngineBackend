ffbbaf8142d656126af9f3f5a3e5a009a37adebd
tang huayi <huayi.tang@etu.unistra.fr>
2023-01-10 01:46:57 +0100

    ONLY FINISHED THE keyword ssearch

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from .search import *
 # Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def get_queryset(self):
+        q = self.request.query_params.get('q')
+        if q:
+            return search_by_keyword(q)
+        return super().get_queryset()
+
+
--- searchEngine/urls.py
+++ searchEngine/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+
+from books.views import BookListView #, BookDetailView , BookSearchView , BookSuggestionView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #GET /search?q=<keyword>
+    path("api/search/", BookListView.as_view() , name="search"),
 ]
