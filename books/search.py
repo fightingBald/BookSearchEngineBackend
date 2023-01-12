68b57c5d6d87a17078c81f8dd2a6a5abf17f796c
tang huayi <huayi.tang@etu.unistra.fr>
2023-01-12 14:57:22 +0100

    DERNIER VERSION QUI MARCHE

--- books/search.py
+++ books/search.py
@@ -6,56 +6,83 @@ from elasticsearch_dsl import connections
 from elasticsearch_dsl import Search
 
 
-# Search documents by keyword. On user input a string S, the application returns a list of  book documents which S appears in the his field 'title', 'author','bookshelf', 'text',`
+
+def create_connection_to_index_books():
+    connections.create_connection(hosts=['elasticsearch:9200']) #TODO change to the service name in docker-compose.yml
+    s = Search(using='default', index='books')
+    return s
+
 
 def search_by_keyword(keyword):
-    # Create the Elasticsearch client
-    connections.create_connection(hosts=['elasticsearch:9200']) #TODO change to the service name in docker-compose.yml
-    # Create the search object
-    s = Search(using='default', index='books')
-    # Create the query and returns the list of documents ordered by relevance, i.e., the score
-    query = Q( "multi_match", query=keyword, fields=['title', 'author', 'bookshelf', 'text'], fuzziness='AUTO', operator='and', ) # Execute the search
-    response = s.query(query ).execute() #Execute the search
+    s = create_connection_to_index_books()
+    # Create the query
+    query = Q(
+        "multi_match",
+        query=keyword,
+        fields=["title", "author", "bookshelf", "text"],
+        type="cross_fields",
+        operator="and"
+    )
+    # Execute the search
+    response = s.query(query).execute()  # Execute the search
     # Return the results
     return response
 
-#— Explicit feature “Advanced search” : Search documents by RegEx. On user input a string RegEx, the application
-#returns : either a list of text documents whose index table contains a string S matching RegEx as regular expression (refer to Lecture 1 of UE DAAR for a formal definition of regular expressions); or a list of text documents
-#containing a string S matching RegEx as regular expression (Warning : this option may cause the application to
-#slow down considerably).
 
+#each book object's text field contains a large amount of text,
+# it may be computationally expensive to perform a regex search on the
+# entire text field for each book.
+#ndexing a subset of the text field, for example, the first few hundred or thousand words,
+# rather than the entire text field.
 def search_by_regex(regex):
-    # Create the Elasticsearch client
-    connections.create_connection(hosts=['elasticsearch:9200']) #TODO change to the service name in docker-compose.yml
-    # Create the search object
-    s = Search(using='default', index='books')
+    s = create_connection_to_index_books()
+    #index the first 1000 words of the text field for each book and then use the search_by_regex(regex) function to search on that indexed subset.
     # Create the query
     query = Q(
         "regexp",
-        text=regex,
+        text={
+            "value": regex,
+            "flags": "ALL",
+            "max_determinized_states": 10000,
+            "rewrite": "constant_score"
+        }
     )
     # Execute the search
     response = s.query(query).execute() #Execute the search
     # Return the results
     return response
 
-def search_by_suggestion(keyword):
-    # Create the Elasticsearch client
-    connections.create_connection(hosts=['elasticsearch:9200']) #TODO change to the service name in docker-compose.yml
-    # Create the search object
-    s = Search(using='default', index='books')
+
+
+
+
+def search_by_suggestion(book_id):
+    s = create_connection_to_index_books()
     # Create the query
     query = Q(
-        "multi_match",
-        query=keyword,
-        fields=['title', 'author', 'bookshelf', 'text'],
-        fuzziness='AUTO',
-        operator='and',
+        "more_like_this",
+        fields=["text"],
+        like= {"_id" : book_id},
+        min_term_freq=1,
+        max_query_terms=12,
     )
     # Execute the search
-    response = s.query(query).suggest('my-suggestion', keyword, term={'field': 'text'}).execute()#
+    response = s.query(query).execute()[1:6] #Execute the search
     # Return the results
     return response
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
